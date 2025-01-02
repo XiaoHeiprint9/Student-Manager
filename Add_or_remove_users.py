4ad2e13420edfdfc6875406ed5edb97d2f9858d8
XiaoHeiprint9 <Xiao.Hei@hotmail.com>
2025-01-02 22:04:58 +0800

    添加功能

--- Add_or_remove_users.py
+++ Add_or_remove_users.py
@@ -1,6 +1,15 @@
-# with open('users.py','w'):
-#     pass
-a=open('users.json','a')
-a.write('n')
-a.close()
-print(a)
+import json
+with open('users.json','w'):
+    pass
+# 定义JSON对象
+data = {
+    "users": [
+        {"name": "Alice", "password": "123456"},
+        {"name": "Bob", "password": "114514"}
+    ]
+}
+
+# 将JSON对象写入文件
+with open('users.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
+
